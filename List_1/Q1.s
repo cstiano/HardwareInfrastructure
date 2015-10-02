135f839926786150d91384994027b9f4b7977a4f
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2015-10-02 20:45:17 -0300

    comments

--- List_1/Q1.s
+++ List_1/Q1.s
@@ -7,14 +7,14 @@ m: .word 10
 .globl start
 .ent start
 start:
-	lw $8, a
-	lw $9, b
- 	lw $10, a
- 	sub $11, $10,$9
- 	bltz $11, label 
- 	move $10,$9
+	lw $8, a #Load memory a
+	lw $9, b #Load memory b
+ 	lw $10, a #Load a again (reg10 = a)
+ 	sub $11, $10,$9 # a - b
+ 	bltz $11, label # == 0? Jump label
+ 	move $10,$9 #Else, (reg10 = b)
  	label:
- 	sw $10,m
+ 	sw $10,m #Sotore reg10 on m
  	nop
  	break	
 .end start
